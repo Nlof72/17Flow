0855e609e0b37a93e87720c40adcb841de0a007d
Nikita Lysov <nlof142@gmail.com>
2021-06-10 16:19:18 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.Modeling = new System.Windows.Forms.Button();
+            this.Lambda1 = new System.Windows.Forms.NumericUpDown();
+            this.Lambda2 = new System.Windows.Forms.NumericUpDown();
+            this.N = new System.Windows.Forms.NumericUpDown();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.T = new System.Windows.Forms.NumericUpDown();
+            this.MeanDisc = new System.Windows.Forms.Label();
+            this.VarDisc = new System.Windows.Forms.Label();
+            this.ChiSquareDisc = new System.Windows.Forms.Label();
+            this.MeanCon = new System.Windows.Forms.Label();
+            this.VarCon = new System.Windows.Forms.Label();
+            this.ChiSquareCon = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Lambda1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lambda2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.N)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.T)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Modeling
+            // 
+            this.Modeling.Location = new System.Drawing.Point(12, 192);
+            this.Modeling.Name = "Modeling";
+            this.Modeling.Size = new System.Drawing.Size(105, 41);
+            this.Modeling.TabIndex = 0;
+            this.Modeling.Text = "Modeling";
+            this.Modeling.UseVisualStyleBackColor = true;
+            this.Modeling.Click += new System.EventHandler(this.Modeling_Click);
+            // 
+            // Lambda1
+            // 
+            this.Lambda1.DecimalPlaces = 2;
+            this.Lambda1.Location = new System.Drawing.Point(12, 12);
+            this.Lambda1.Name = "Lambda1";
+            this.Lambda1.Size = new System.Drawing.Size(120, 22);
+            this.Lambda1.TabIndex = 1;
+            this.Lambda1.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            65536});
+            // 
+            // Lambda2
+            // 
+            this.Lambda2.DecimalPlaces = 2;
+            this.Lambda2.Location = new System.Drawing.Point(12, 53);
+            this.Lambda2.Name = "Lambda2";
+            this.Lambda2.Size = new System.Drawing.Size(120, 22);
+            this.Lambda2.TabIndex = 2;
+            this.Lambda2.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            65536});
+            // 
+            // N
+            // 
+            this.N.Location = new System.Drawing.Point(12, 98);
+            this.N.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(120, 22);
+            this.N.TabIndex = 3;
+            this.N.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chartArea2.Name = "ChartArea2";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(346, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series6";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series8";
+            series3.ChartArea = "ChartArea2";
+            series3.Legend = "Legend1";
+            series3.Name = "Series7";
+            series4.ChartArea = "ChartArea2";
+            series4.Legend = "Legend1";
+            series4.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(930, 594);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            title1.DockedToChartArea = "ChartArea1";
+            title1.Name = "Title1";
+            title1.Text = "Discrete";
+            title2.DockedToChartArea = "ChartArea2";
+            title2.Name = "Title2";
+            title2.Text = "Continuous";
+            this.chart1.Titles.Add(title1);
+            this.chart1.Titles.Add(title2);
+            // 
+            // T
+            // 
+            this.T.Location = new System.Drawing.Point(12, 140);
+            this.T.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(120, 22);
+            this.T.TabIndex = 5;
+            this.T.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // MeanDisc
+            // 
+            this.MeanDisc.AutoSize = true;
+            this.MeanDisc.Location = new System.Drawing.Point(37, 270);
+            this.MeanDisc.Name = "MeanDisc";
+            this.MeanDisc.Size = new System.Drawing.Size(43, 17);
+            this.MeanDisc.TabIndex = 6;
+            this.MeanDisc.Text = "Mean";
+            // 
+            // VarDisc
+            // 
+            this.VarDisc.AutoSize = true;
+            this.VarDisc.Location = new System.Drawing.Point(37, 323);
+            this.VarDisc.Name = "VarDisc";
+            this.VarDisc.Size = new System.Drawing.Size(30, 17);
+            this.VarDisc.TabIndex = 7;
+            this.VarDisc.Text = "Var";
+            // 
+            // ChiSquareDisc
+            // 
+            this.ChiSquareDisc.AutoSize = true;
+            this.ChiSquareDisc.Location = new System.Drawing.Point(37, 387);
+            this.ChiSquareDisc.Name = "ChiSquareDisc";
+            this.ChiSquareDisc.Size = new System.Drawing.Size(55, 17);
+            this.ChiSquareDisc.TabIndex = 8;
+            this.ChiSquareDisc.Text = "ChiDisc";
+            // 
+            // MeanCon
+            // 
+            this.MeanCon.AutoSize = true;
+            this.MeanCon.Location = new System.Drawing.Point(37, 447);
+            this.MeanCon.Name = "MeanCon";
+            this.MeanCon.Size = new System.Drawing.Size(68, 17);
+            this.MeanCon.TabIndex = 9;
+            this.MeanCon.Text = "MeanCon";
+            // 
+            // VarCon
+            // 
+            this.VarCon.AutoSize = true;
+            this.VarCon.Location = new System.Drawing.Point(37, 500);
+            this.VarCon.Name = "VarCon";
+            this.VarCon.Size = new System.Drawing.Size(55, 17);
+            this.VarCon.TabIndex = 10;
+            this.VarCon.Text = "VarCon";
+            // 
+            // ChiSquareCon
+            // 
+            this.ChiSquareCon.AutoSize = true;
+            this.ChiSquareCon.Location = new System.Drawing.Point(37, 550);
+            this.ChiSquareCon.Name = "ChiSquareCon";
+            this.ChiSquareCon.Size = new System.Drawing.Size(51, 17);
+            this.ChiSquareCon.TabIndex = 11;
+            this.ChiSquareCon.Text = "chiCon";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "DiscreteStats";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 421);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "ContinuousStats";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1277, 618);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ChiSquareCon);
+            this.Controls.Add(this.VarCon);
+            this.Controls.Add(this.MeanCon);
+            this.Controls.Add(this.ChiSquareDisc);
+            this.Controls.Add(this.VarDisc);
+            this.Controls.Add(this.MeanDisc);
+            this.Controls.Add(this.T);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.Lambda2);
+            this.Controls.Add(this.Lambda1);
+            this.Controls.Add(this.Modeling);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Lambda1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lambda2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.N)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.T)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Modeling;
+        private System.Windows.Forms.NumericUpDown Lambda1;
+        private System.Windows.Forms.NumericUpDown Lambda2;
+        private System.Windows.Forms.NumericUpDown N;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown T;
+        private System.Windows.Forms.Label MeanDisc;
+        private System.Windows.Forms.Label VarDisc;
+        private System.Windows.Forms.Label ChiSquareDisc;
+        private System.Windows.Forms.Label MeanCon;
+        private System.Windows.Forms.Label VarCon;
+        private System.Windows.Forms.Label ChiSquareCon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        double t;
+        double lambda1;
+        double lambda2;
+        Random rnd;
+
+        int[] Prob1;    
+        int[] Prob2;
+        List<double> Line1 = new List<double>();
+        List<double> Line2 = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            rnd = new Random();
+        }
+
+        private void Modeling_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            chart1.Series[3].Points.Clear();
+
+            n = (int)N.Value;
+            t = (double)T.Value;
+            lambda1 = (double)Lambda1.Value;
+            lambda2 = (double)Lambda2.Value;
+
+            Prob1 = new int[n];
+            Prob2 = new int[n];
+
+            Simulate();
+
+            var hists = Histogramm();
+            WriteStat(hists);
+            Stat(hists);
+
+        }
+
+        private double Exponential(double lambda)
+        {
+            double a = rnd.NextDouble();
+            return -Math.Log(a) / lambda;
+        }
+        void Simulate()
+       {
+            double TimeStart = 0;
+            double time1 = 0, time2 = 0;
+            double a, f = 0; ;
+            for (int i = 0; i < n; i++)
+            {
+                int k = 0;
+                while (TimeStart < t)
+                {
+                    if (f == 0)
+                    {
+                        time1 += Exponential(lambda1);
+                        time2 += Exponential(lambda2);
+                    }
+                    else if (f == 2)
+                    {
+                        time2 += Exponential(lambda2);
+                    }
+                    else if (f == 1)
+                    {
+                        time1 += Exponential(lambda1);
+                    }
+                    if (time1 > time2)
+                    {
+                        Line1.Add(time2 - TimeStart);
+                        TimeStart = time2;
+                        f = 2;
+                    }
+                    else
+                    {
+                        Line1.Add(time1 - TimeStart);
+                        TimeStart = time1;
+                        f = 1;
+                    }
+                    k++;
+                }
+                TimeStart = 0;
+                time1 = 0;
+                time2 = 0;
+                f = 0;
+                Prob1[k]++;
+            }
+            TimeStart = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int k = 0;
+                while (TimeStart < t)
+                {
+                    time1 = Exponential(lambda1+lambda2);
+                    Line2.Add(time1);
+                    TimeStart += time1;
+                    k++;
+                }
+                TimeStart = 0;
+                Prob2[k]++;
+            }
+        }
+
+
+        (double[],double[]) Histogramm()
+        {
+            double[] histo1 = new double[10];
+            double[] histo2 = new double[10];
+            int min1 = 0, max1 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if ((Prob1[i] != 0 || Prob2[i] != 0) && min1 == 0) min1 = i;
+                else if ((Prob1[i] != 0 || Prob2[i] != 0)) max1 = i;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = (max1 - min1) / 10 * i + min1; j < (max1 - min1) / 10 * (i + 1) + min1; j++)
+                {
+                    histo1[i] += Prob1[j];
+                    histo2[i] += Prob2[j];
+                }
+                histo1[i] = histo1[i] / n;
+                histo2[i] = histo2[i] / n;
+                chart1.Series[0].Points.AddXY(i + 1, histo1[i]);
+                chart1.Series[1].Points.AddXY(i + 1, histo2[i]);
+            }
+
+            return (histo1, histo2);
+        }
+
+
+
+        void WriteStat((double[], double[]) Hists)
+        {
+            double E1 = 0, E2 = 0, Var1 = 0, Var2 = 0;
+            for (int i = 0; i < 10; i++)
+                E1 += i * Hists.Item1[i];
+            for (int i = 0; i < 10; i++)
+                E2 += i * Hists.Item2[i];
+            for (int i = 0; i < 10; i++)
+                Var1 += i * i * Hists.Item1[i];
+            for (int i = 0; i < 10; i++)
+                Var2 += i * i * Hists.Item2[i];
+            Var1 -=E1 * E1;
+            Var2 -=E1 * E1;
+
+            MeanDisc.Text = "Mean: " + Math.Round(E1, 2) + "\n(error = " + Math.Round(Math.Abs(((E1 - E2) / E1) * 100), 2) + "%)";
+            VarDisc.Text = "Variance: " + Math.Round(Var1, 2) + "\n(error = " + Math.Round(Math.Abs(((Var1 - Var2) / Var1) * 100), 2) + "%)";
+
+
+            double Ev = 0, Varv = 0, En = 0, Varn = 0;
+            for (int i = 0; i < Line1.Count; i++)
+                En += Line1[i] / Line1.Count;
+            for (int i = 0; i < Line1.Count; i++)
+                Varn += Line1[i] * Line1[i] / Line1.Count - En * En / Line1.Count;
+            for (int i = 0; i < Line2.Count; i++)
+                Ev += Line2[i] / Line2.Count;
+            for (int i = 0; i < Line2.Count; i++)
+                Varv += Line2[i] * Line2[i] / Line2.Count - Ev * Ev / Line2.Count;
+
+            MeanCon.Text = "Mean: " + Math.Round(En, 2) + "\n(error = " + Math.Round(Math.Abs((Math.Abs((Ev - En)) / Ev) * 100), 2) + "%)";
+            VarCon.Text = "Variance: " + Math.Round(Varn, 2) + "\n(error = " + Math.Round(Math.Abs(((Varv - Varn) / Varv) * 100), 2) + "%)";
+
+
+        }
+
+        void ChiStat((double[], double[]) Hists, (double[], double[]) Hists2, int inter)
+        {
+            double Chi = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (Hists.Item2[i] != 0)
+                    Chi += n * (Hists.Item1[i] - Hists.Item2[i]) * (Hists.Item1[i] - Hists.Item2[i]) / Hists.Item2[i];
+            }
+            double ChiCalc = alglib.invchisquaredistribution(9, 0.01);
+
+            var histogram = Hists2.Item1;
+            var histogram1 = Hists2.Item2;
+            ChiSquareDisc.Text = "Chi-squared:\n" + Math.Round(Chi, 2) + " > " + Math.Round(ChiCalc, 2);
+            if (ChiCalc > Chi) ChiSquareDisc.Text += "\nFalse";
+            else ChiSquareDisc.Text += "\nTrue";
+
+
+            double ChiContin = 0;
+            for (int i = 0; i < inter; i++)
+            {
+                if (histogram1[i] != 0)
+                    ChiContin += (histogram[i] - histogram1[i]) * (histogram[i] - histogram1[i]) / (histogram1[i]);
+            }
+            ChiCalc = alglib.invchisquaredistribution(inter, 0.01);
+
+            ChiSquareCon.Text = "Chi-squared:\n" + Math.Round(ChiContin, 2) + " > " + Math.Round(ChiCalc, 2);
+            if (ChiCalc > ChiContin) ChiSquareCon.Text += "\nFalse";
+            else ChiSquareCon.Text += "\nTrue";
+
+        }
+
+
+        (int, double,double) InterMinProm(List<double> line)
+        {
+            double min = 999999999999999;
+            double max = 0;
+            for (int i = 0; i < line.Count; i++)
+            {
+                if (line[i] > max) max = line[i];
+                if (line[i] < min) min = line[i];
+            }
+
+            int inter = (int)Math.Ceiling(Math.Log(line.Count, 2)) + 1;
+            double prom = (max - min) / inter;
+            return (inter,min,prom);
+        }
+
+        void Stat((double[], double[]) Hists)
+        {
+            var stat = InterMinProm(Line1);
+            double min = stat.Item2;
+            int inter = stat.Item1;
+            double prom = stat.Item3;
+            double temp_max = min +  prom;
+            min -= 0.0000001;
+            double temp_min = min;
+            double[] histogram = new double[inter];
+
+            for (int i = 0; i < inter; i++)
+            {
+                histogram[i] = 0;
+                for (int j = 0; j < Line1.Count; j++)
+                    if (Line1[j] > temp_min && Line1[j] <= temp_max)
+                        histogram[i]++;
+                chart1.Series[2].Points.AddXY("( " + Math.Round(temp_min, 1) + " : " + Math.Round(temp_max, 1) + " ]", histogram[i] / Line1.Count);
+                temp_min = temp_max;
+                temp_max += prom;
+            }
+
+            stat = InterMinProm(Line2);
+            min = stat.Item2;
+            inter = stat.Item1;
+            prom = stat.Item3;
+            min -= 0.0000001;
+
+            double[] histogram1 = new double[inter];
+
+            temp_max = min + prom;
+            min -= 0.0000001;
+            temp_min = min;
+            for (int i = 0; i < inter; i++)
+            {
+                histogram1[i] = 0;
+                for (int j = 0; j < Line2.Count; j++)
+                    if (Line2[j] > temp_min && Line2[j] <= temp_max)
+                        histogram1[i]++;
+                chart1.Series[3].Points.AddXY("( " + Math.Round(temp_min, 1) + " : " + Math.Round(temp_max, 1) + " ]", histogram1[i] / Line2.Count);
+                temp_min = temp_max;
+                temp_max += prom;
+            }
+            var a = (histogram, histogram1);
+            ChiStat(Hists, a, inter);
+            //return ;
+        }
+    }
+}
